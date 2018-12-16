3361636dafa4b65c88d8f2836b37408258ef2a75
atacai <ata.chai10@gmail.com>
2018-12-16 13:01:02 +0700

    change routes add resources of controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'homes#index'
+  resources :conferences do
+		collection do
+			get :change_priority
+		end
+	end
+	resources :proposals
+	resources :session_configurations
 end
